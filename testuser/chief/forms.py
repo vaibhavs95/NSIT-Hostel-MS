651a8add2ef489d38cfe5d9830b4664b569cc26c
shivamkantival <shivam.kantival@yahoo.co.in>
2016-03-22 12:11:59 +0530

    changes in model, please do migrate

--- testuser/chief/forms.py
+++ testuser/chief/forms.py
@@ -5,22 +5,18 @@ from newapp.models import *
 import re
 
 class CreateWardenForm(forms.Form):
-    userid = forms.CharField(max_length = 254)
-    password = forms.CharField(widget = forms.PasswordInput)
-    retype_password = forms.CharField(widget = forms.PasswordInput)
+    userid = forms.CharField(max_length = 254,required = True)
+    password = forms.CharField(widget = forms.PasswordInput,required = True)
+    retype_password = forms.CharField(widget = forms.PasswordInput,required = True)
     def __init__(self, *args, **kwargs):
         self.user_cache = None
         super(CreateWardenForm, self).__init__(*args, **kwargs)
     
     def clean_userid(self):
         userid = self.cleaned_data.get('userid')
-        if not re.match("[bh]h[0-9]*warden",userid):
+        userid = userid.lower()
+        if not re.match("[bg]h[0-9]*warden",userid):
             raise forms.ValidationError('Not a correct format for this field')
-        return self.cleaned_data
-    
-    def clean(self):
-        userid = self.cleaned_data.get('userid')
-        password = self.cleaned_data.get('password')
         h = None
         m = None
         try:
@@ -30,24 +26,25 @@ class CreateWardenForm(forms.Form):
         	pass
         if h is not None or m is not None:
         	raise forms.ValidationError('User Alredy Exists')
-        return self.cleaned_data
+        return userid
+    
     def clean_retype_password(self):
         p = self.cleaned_data.get('password')
         q = self.cleaned_data.get('retype_password')
         if not q==p:
             raise forms.ValidationError('Passwords do not match')
-    
+        return q
     def get_user(self):
         return self.user_cache
 
 class AddBranchForm(forms.Form):
     title = forms.CharField(max_length = 5)
     name = forms.CharField(max_length = 100)
+    Roll_Code = forms.CharField(max_length = 5,required = True)
     def __init__(self, *args, **kwargs):
         super(AddBranchForm, self).__init__(*args, **kwargs)
-    def clean(self):
+    def clean_title(self):
         title = self.cleaned_data.get('title')
-        name = self.cleaned_data.get('name')
         title = title.upper()
         b = None
         try:
@@ -56,4 +53,29 @@ class AddBranchForm(forms.Form):
             pass
         if b is not None:
             raise forms.ValidationError('Branch Already Exists')
-        return self.cleaned_data
+        return title
+    
+    def clean_name(self):
+        
+        name = self.cleaned_data.get('name')
+        name = name.upper()
+        b = None
+        try:
+            b = Branch.objects.get(name=name)
+        except ObjectDoesNotExist:
+            pass
+        if b is not None:
+            raise forms.ValidationError('This code Already Exists')
+        return name
+    def clean_Roll_Code(self):
+        
+        Roll_Code= self.cleaned_data.get('Roll_Code')
+        Roll_Code= Roll_Code.upper()
+        b = None
+        try:
+            b = Branch.objects.get(roll_code=Roll_Code)
+        except ObjectDoesNotExist:
+            pass
+        if b is not None:
+            raise forms.ValidationError('This code Already Exists')
+        return Roll_Code
--- testuser/chief/views.py
+++ testuser/chief/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 import re
-
 from django.shortcuts import render,redirect, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
@@ -13,91 +12,105 @@ from .forms import *
 @login_required
 @require_http_methods(['GET', 'POST'])
 def home(request):
-    a=Hostels.objects.all();
-    b=[]
-    for i in a:
-        d={'name':i.hostel_name,'id':i.username,'warden_name':i.name,'warden_nu':i.phone}
-        b.append(d)
     alpha = str(request.user)
     if alpha =='chiefwarden':
-        data = {'all_hostels': b}
+        f = CreateWardenForm(request.POST or None)
+        if f.is_valid():
+            user = MyUser.objects.create_user(f.cleaned_data.get('userid'), '2016-02-02', f.cleaned_data.get('password'))
+            user.save()
+            warden = Hostels(username = f.cleaned_data.get('userid'))
+            warden.chief_warden = ChiefWarden.objects.all()[0]
+            hos = str(f.cleaned_data.get('userid'))
+            fo,m = hos.split('w')
+            fo,m = fo.split('h')
+            if fo=='b':
+                warden.hostel_name = "Boys Hostel "+m
+            elif fo=='g':
+                warden.hostel_name= "Girls Hostel "+m
+            print(warden,'''cjkadcjhcjhcjkhcujkchvjkshbvjksdcbvkjjsvkcbvjkchbv
+                  c jcb skjcvbjscb jhbcvhjscbvhjscbvjsdbvjsdiusdvbisdvbg''')
+            warden.save()
+        a=Hostels.objects.all();
+        b=[]
+        for i in a:
+            d={'name':i.hostel_name,'id':i.username,'warden_name':i.name,'warden_nu':i.phone}
+            b.append(d)
+        data = {'all_hostels': b,'form':f}
         return render(request, 'chief/home.html',data)
     else:
         return redirect('logout')
-@login_required
-@require_http_methods(['GET', 'POST'])
-def add_hos(request):
-    a=Hostels.objects.all();
-    b=[]
-    for i in a:
-        d={'name':i.hostel_name,'id':i.username}
-        b.append(d)
-    alpha = str(request.user)
-    if alpha =='chiefwarden':
-        if request.method == 'POST':
-            fo = CreateWardenForm(request.POST or None)
-            if fo.is_valid():
-                user = MyUser.objects.create_user(fo.cleaned_data.get('userid'), '2016-02-02', fo.cleaned_data.get('password'))
-                user.save()
-                warden = Hostels(username = fo.cleaned_data.get('userid'))
-                warden.chief_warden = ChiefWarden.objects.all()[0]
-                hos = fo.cleaned_data.get('userid')
-                f,m = hos.split('w')
-                f,m=f.split('h')
-                if f=='b':
-                    warden.hostel_name = "Boys Hostel "+m
-                elif f=='g':
-                    warden.hostel_name= "Girls Hostel "+m
-                warden.save()
-                g = AddBranchForm()
-                data = {'form': fo, 'userid': hos,'all_hostels': b,'branchform':g}
-                return render(request, 'chief/add_hostel.html', data)
-            else:
-                g = AddBranchForm()
-                data = {'form': fo, 'useridfail': fo.cleaned_data.get('userid'),'all_hostels': b,'branchform':g}
-                return render(request, 'chief/add_hostel.html', data)
-        else:
-            fo = CreateWardenForm()
-            g = AddBranchForm()
-        data = {'form': fo,'all_hostels': b,'branchform':g}
-        return render(request, 'chief/add_hostel.html',data)
-    else:
-        return redirect('logout')
+
 @login_required
 @require_http_methods(['GET', 'POST'])
 def addbranch(request):
-    if request.method == 'POST':
+    alpha = str(request.user)
+    if alpha =='chiefwarden':
         fb = AddBranchForm(request.POST or None)
+        mes = None
         if fb.is_valid():
-            print(fb.cleaned_data.get('title'))
-            b = Branch(title = fb.cleaned_data.get('title'),name = fb.cleaned_data.get('name'))
+            b = Branch(title = fb.cleaned_data.get('title'),name = fb.cleaned_data.get('name'),roll_code=fb.cleaned_data.get('Roll_Code'))
             b.save()
-        g = CreateWardenForm()
-        data = {'branchform': fb,'form':g}
-        return render(request,'chief/add_hostel.html',data)
+            mes = 'Branch added successfully.'
+        a=Hostels.objects.all();
+        b=[]
+        for i in a:
+            d={'name':i.hostel_name,'id':i.username,'warden_name':i.name,'warden_nu':i.phone}
+            b.append(d)
+        a= Branch.objects.all()
+        data = {'all_branch':a,'form':fb,'mes':mes,'all_hostels':b}        
+        return render(request,'chief/branches.html',data)
     else:
-        g = CreateWardenForm()
-        f = AddBranchForm()
-        data = {'branchform': f,'form':g}
-        return render(request,'chief/add_hostel.html',data)
+        return redirect('logout')
+
 @login_required
 @require_http_methods(['GET', 'POST'])
 def delete_hos(request,target):
-    a=Hostels.objects.all();
-    b=[]
-    for i in a:
-        d={'name':i.hostel_name,'id':i.username}
-        b.append(d)
     alpha = str(request.user)
     if alpha =='chiefwarden':
         try:
             a=Rooms.objects.filter(hostel = target)
         except ObjectDoesNotExist:
             pass
+        j=0
+        mes = None
         for i in a:
-            if i.filled_rooms != 0:             
-                messages.warning(request, 'Your account expires in three days.')
-        data = {'all_hostels': b}
+            if i.capacity_remaining != i.capacity_of_room:
+                mes="Hostel still has some rooms alloted and thus can't be deleted."
+                j=1
+                break
+        if j==0:
+            a.delete()
+            try:
+                a = Hostels.objects.filter(username = target)
+            except ObjectDoesNotExist:
+                pass
+            a.delete()
+            try:
+                a = MyUser.objects.get(userid = target)
+            except ObjectDoesNotExist:
+                pass
+            a.delete()
+            mes="Hostel deleted successfully."
+        f=CreateWardenForm(request.POST or None)
+        if f.is_valid():
+            user = MyUser.objects.create_user(f.cleaned_data.get('userid'), '2016-02-02', f.cleaned_data.get('password'))
+            user.save()
+            warden = Hostels(username = f.cleaned_data.get('userid'))
+            warden.chief_warden = ChiefWarden.objects.all()[0]
+            hos = f.cleaned_data.get('userid')
+            fo,m = str(hos.split('w'))
+            fo,m=f.split('h')
+            if fo=='b':
+                warden.hostel_name = "Boys Hostel "+m
+            elif fo=='g':
+                warden.hostel_name= "Girls Hostel "+m
+            warden.save()
+        a=Hostels.objects.all();
+        b=[]
+        for i in a:
+            d={'name':i.hostel_name,'id':i.username}
+            b.append(d)
+        data = {'all_hostels': b,'mes':mes,'form':f}
         return render(request, 'chief/home.html',data)
     else:
         return redirect('logout')
