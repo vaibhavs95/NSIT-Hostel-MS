4c8d2ccb119090fd76b6e9964e844b86ba5e8120
vaibhav singh <vaibhav.singh911@gmail.com>
2016-04-01 00:59:33 +0530

    qwer

--- testuser/testuser/settings.py
+++ testuser/testuser/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'testuser',
         'USER' : 'root',
         'HOST' : 'localhost', 
-        'PASSWORD' : 'howareyou',
+        'PASSWORD' : '12345',
         'PORT' : 3306
     }
 }
