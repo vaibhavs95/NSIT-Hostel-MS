3db2869b8ec92f84e3428a0cd920273153b69460
shivamkantival <shivam.kantival@yahoo.co.in>
2016-04-04 13:09:11 +0530

    Reset Password Working

---
+++ testuser/newapp/migrations/0009_auto_20160404_1237.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('newapp', '0008_auto_20160403_2039'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='myuser',
+            name='date_of_birth',
+            field=models.DateField(default=datetime.datetime(2016, 4, 4, 7, 7, 32, 341437, tzinfo=utc)),
+        ),
+    ]
